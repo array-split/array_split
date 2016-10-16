e2a9fdd0ab617b55e839839f6c40aa7b9bdc5258
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2016-10-16 17:36:46 +1100

    autopep8 and add license and copyright to array_split.__main__.

--- array_split/__main__.py
+++ array_split/__main__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
-
+from __future__ import absolute_import
 import array_split
 import array_split.split
+from array_split.license import license as _license
+from array_split.license import copyright as _copyright
+
+__author__ = "Shane J. Latham"
+__license__ = _license()
+__copyright__ = _copyright()
+__version__ = array_split.__version__
+
 
 if __name__ == "__main__":
     import doctest
