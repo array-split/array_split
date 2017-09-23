920b4c2cee3ec37b115a190f5dbae0d2e56ec26a
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2017-09-23 10:04:18 +1000

    Add skeleton for plotting split.

---
+++ array_split/split_plot.py
@@ -0,0 +1,43 @@
+"""
+========================================
+The :mod:`array_split.split_plot` Module
+========================================
+
+Uses :mod:`matplotlib` to plot a split.
+
+Classes and Functions
+=====================
+
+.. autosummary::
+   :toctree: generated/
+
+   SplitPlotter - Plots a split.
+   plot - Plots split shapes.
+
+"""
+
+from __future__ import absolute_import
+
+from .license import license as _license, copyright as _copyright, version as _version
+
+__author__ = "Shane J. Latham"
+__license__ = _license()
+__copyright__ = _copyright()
+__version__ = _version()
+
+
+class SplitPlotter(object):
+    """
+    Plots a split.
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+
+def plot(split):
+    """
+    Plots a split.
+    """
+
